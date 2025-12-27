35d9465bcc6999b506aeea013545d530d7597738
Zamudio <61006554+jzamudio1@users.noreply.github.com>
2025-12-27 17:04:51 +0100

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://jzamudio1.github.io',
-  base: '/porfolio'
+  site: 'https://jzamudio1.github.io'
 })
